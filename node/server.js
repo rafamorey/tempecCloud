fdef7317cf9b7805d553c536495c5a0b96edb08a
Rafael <rafaelmreyes@hotmail.com>
2022-07-19 17:48:24 -0700

    creando rutas get

--- node/server.js
+++ node/server.js
@@ -1,11 +1,33 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router()
 
 var app = express();
+app.use(bodyParser.json())
+app.use(router)
 
-app.use('/', (req, res)=>
-  res.send('Hola')
-)
+router.get('/message', (req, res) =>{
+  console.log(req.header)
+  res.header({
+    "customHeader": "Nuestro valor"
+  })
+  res.send('Hola desde get')
+})
+
+router.post('/message', (req, res) =>{
+  res.status(201).send({
+    "error": "",
+    "body": "creado correctamente"
+  })
+})
+
+router.delete('/message', (req, res) =>{
+  console.log(req.body)
+  console.log(req.query)
+  res.send('mensaje ' + req.body.text + ' borrado exitosamente')
+})
+
 
 app.listen(3000)
 console.log('la app esta escuchando en el http://localhost:3000')
