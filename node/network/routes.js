5a2bb09381b4d9ba3135e3f49c3d4e095e49ed7f
Monzav <rmonzav@gmail.com>
2022-08-11 12:47:10 -0500

    limpieza

--- node/network/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const message =require('../components/message/message/networks')
-const routes = function(server) {
-  server.use('/message', message)
-}
-
-module.exports = routes
