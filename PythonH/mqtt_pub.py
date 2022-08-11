0ac7dd0c8cebb90e3c4ed7536287876440ec15ee
Rafa morey <rafaelmreyes@hotmail.com>
2022-08-11 12:44:45 -0700

    borrando datos rafa monje

--- PythonH/mqtt_pub.py
+++
@@ -1,8 +0,0 @@
-from http import client
-'''
-import paho.mqtt.client as mqtt
-
-cliente = mqtt.Client()
-cliente.connect("test.mosquitto.org", 1883, 60)
-cliente.publish("Monzav/Server", "Hola Corazon")
-'''
